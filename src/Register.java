47e38c8351f6b23632bb09f626504788b1bc6a4d
Aaron <1187502160@qq.com>
2016-02-19 12:35:04 -0800

    Update Register.java

--- src/Register.java
+++ src/Register.java
@@ -37,7 +37,7 @@ public class Register {
 	public void connect(){
 		try{
 			Class.forName("com.mysql.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/login?useUnicode=true&characterEncoding=utf-8&useSSL=false","root","qazZAQ360921,.");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/login?useUnicode=true&characterEncoding=utf-8&useSSL=false","root","password");
 			st = con.createStatement();
 			
 		}catch(Exception e){
